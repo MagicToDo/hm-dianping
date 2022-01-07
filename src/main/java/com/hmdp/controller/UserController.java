21b0ce2fd312ec5cec88ea1e2c37e33d5ac8c7b4
虎哥 <huyi0612@163.com>
2022-01-07 15:27:56 +0800

    初始化

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -37,7 +37,7 @@ public class UserController {
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
         // TODO 发送短信验证码并保存验证码
-        return Result.ok();
+        return Result.fail("功能未完成");
     }
 
     /**
