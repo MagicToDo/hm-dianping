2b2bc042df819e896325097b5d9b06cbe0637707
虎哥 <huyi0612@163.com>
2022-01-25 10:59:02 +0800

    01-缓存解决方案-缓存同步、穿透、击穿、工具类

--- src/main/java/com/hmdp/controller/VoucherController.java
+++ src/main/java/com/hmdp/controller/VoucherController.java
@@ -24,6 +24,17 @@ public class VoucherController {
     private IVoucherService voucherService;
 
     /**
+     * 新增秒杀券
+     * @param voucher 优惠券信息，包含秒杀信息
+     * @return 优惠券id
+     */
+    @PostMapping("seckill")
+    public Result addSeckillVoucher(@RequestBody Voucher voucher) {
+        voucherService.addSeckillVoucher(voucher);
+        return Result.ok(voucher.getId());
+    }
+
+    /**
      * 新增普通券
      * @param voucher 优惠券信息
      * @return 优惠券id
@@ -34,16 +45,6 @@ public class VoucherController {
         return Result.ok(voucher.getId());
     }
 
-    /**
-     * 新增秒杀券
-     * @param voucher 优惠券信息，包含秒杀信息
-     * @return 优惠券id
-     */
-    @PostMapping("seckill")
-    public Result addSeckillVoucher(@RequestBody Voucher voucher) {
-        voucherService.addSeckillVoucher(voucher);
-        return Result.ok(voucher.getId());
-    }
 
     /**
      * 查询店铺的优惠券列表
--- src/main/java/com/hmdp/entity/VoucherOrder.java
+++ src/main/java/com/hmdp/entity/VoucherOrder.java
@@ -1,13 +1,14 @@
 package com.hmdp.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
-import java.time.LocalDateTime;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -28,7 +29,7 @@ public class VoucherOrder implements Serializable {
     /**
      * 主键
      */
-    @TableId(value = "id", type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.INPUT)
     private Long id;
 
     /**
@@ -44,12 +45,12 @@ public class VoucherOrder implements Serializable {
     /**
      * 支付方式 1：余额支付；2：支付宝；3：微信
      */
-    private Boolean payType;
+    private Integer payType;
 
     /**
      * 订单状态，1：未支付；2：已支付；3：已核销；4：已取消；5：退款中；6：已退款
      */
-    private Boolean status;
+    private Integer status;
 
     /**
      * 下单时间
